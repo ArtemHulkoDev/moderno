19bddfbc68c0847dde18cea663fc535cde8d69e7
ArtemGulkoDev <agulko96@gmail.com>
2020-04-14 02:35:45 +0300

    added product-page section

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,5 @@
 $(function(){
 
-  var mixer = mixitup('.products__inner-box');
-
   $(".rate-star").rateYo({
     rating: 5,
     starWidth: "12px",
@@ -14,5 +12,30 @@ $(function(){
     slidesToShow: 4,
     slidesToScroll: 4
   });
+
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$",
+    skin: "round",
+    hide_min_max: true
+  });
+
+  $('.icon-th-list').on('click', function(){
+    $('.product-page__content .product__item').addClass('list');
+    $('.icon-th-large').removeClass('active');
+    $('.icon-th-list').addClass('active');
+  });
+
+  $('.icon-th-large').on('click', function(){
+    $('.product-page__content .product__item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active');
+  });
+
+  var mixer = mixitup('.products__inner-box');
 
 });
